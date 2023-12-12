995c30bc8b598dd2fa3768b79fb0402d2efeb2cc
Rojar Smith <rojarsmith@gmail.com>
2023-12-12 16:04:08 +0800

    feat: `manifest.webmanifest`

---
+++ src/app/manifest.js
@@ -0,0 +1,20 @@
+const manifest = () => {
+    return {
+        name: 'Next.js App',
+        short_name: 'Next.js App',
+        description: 'Next.js App',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#fff',
+        theme_color: '#fff',
+        icons: [
+            {
+                src: '/favicon.ico',
+                sizes: 'any',
+                type: 'image/x-icon',
+            },
+        ],
+    }
+};
+
+export default manifest;
